97a7bf3006e28410e0d108bfc65273c592aef0f9
D1360D4C <diegoarmando117@gmail.com>
2024-04-19 14:57:41 -0300

    mejore la flueidez de movimiento

--- ejercicio5/ejercicio5.js
+++ ejercicio5/ejercicio5.js
@@ -2,6 +2,10 @@ let charizardImg;
 let posX = 200;
 let posY = 200;
 let speed = 5;
+let izq = false;
+let der = false;
+let arr = false;
+let aba = false;
 
 function preload() {
   // Cargar la imagen de Charizard
@@ -17,17 +21,50 @@ function draw() {
   
   // Dibujar la imagen de Charizard en la posición actual
   image(charizardImg, posX, posY, 100, 100);
+
+  if (izq == true){
+    posX -= speed;   
+  }
+  if (der == true){
+    posX += speed; 
+  }
+  if (arr == true){
+    posY -= speed;
+  }
+  if (aba == true){
+    posY += speed;
+  }
+  
 }
 
 function keyPressed() {
   // Mover la imagen según la tecla presionada
   if (keyCode === LEFT_ARROW || key === 'a') {
-    posX -= speed;
-  } else if (keyCode === RIGHT_ARROW || key === 'd') {
-    posX += speed;
-  } else if (keyCode === UP_ARROW || key === 'w') {
-    posY -= speed;
-  } else if (keyCode === DOWN_ARROW || key === 's') {
-    posY += speed;
+    izq = true;
+  } 
+  if (keyCode === RIGHT_ARROW || key === 'd') {
+    der = true;
+  } 
+  if (keyCode === UP_ARROW || key === 'w') {
+    arr = true;
+  } 
+  if (keyCode === DOWN_ARROW || key === 's') {
+    aba = true;
+  }
+}
+
+function keyReleased() {
+  // Mover la imagen según la tecla presionada
+  if (keyCode === LEFT_ARROW || key === 'a') {
+    izq = false;
+  } 
+  if (keyCode === RIGHT_ARROW || key === 'd') {
+    der = false;
+  } 
+  if (keyCode === UP_ARROW || key === 'w') {
+    arr = false;
+  } 
+  if (keyCode === DOWN_ARROW || key === 's') {
+    aba = false;
   }
 }
